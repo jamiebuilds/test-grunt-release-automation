43c03449d1365fa7c0af77c9b6dc5bf4cabccc7e
James Kyle <me@thejameskyle.com>
2014-04-12 21:55:04 -0400

    [WIP]

--- tasks/release.js
+++ tasks/release.js
@@ -8,7 +8,8 @@ var Repo = require('gitty')('./');
 var checkStatusOfRepo = function () {
   return new Promise(function (resolve, reject) {
     Repo.status(function (err, status) {
-      console.log(status);
+      console.log(status.staged.length);
+      console.log(status.not_staged);
       if (status.staged.length !== 0 || status.not_staged !== 0) {
         reject('Please commit all files before performing a release.');
       } else {
