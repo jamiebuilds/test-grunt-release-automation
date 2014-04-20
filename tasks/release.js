97212cda0e0d8ccb5106548c3c6e88defb138d85
James Kyle <me@thejameskyle.com>
2014-04-19 20:36:30 -0400

    [WIP]

--- tasks/release.js
+++ tasks/release.js
@@ -65,7 +65,7 @@ var checkForBadVersion = function() {
 
 var editChangelog = function() {
   console.log('VIM: Edited CHANGELOG');
-  return execAsyncProcess('vim CHANGELOG.md');
+  return execAsyncProcess('subl -w CHANGELOG.md');
 };
 
 var editUpgradeGuide = function() {
